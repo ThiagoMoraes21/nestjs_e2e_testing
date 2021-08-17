40a2a69681332f5c31fe701bfd0d4d97dd68872c
Thiago Moraes <thiago4x70@gmail.com>
2021-08-17 11:43:22 -0300

    add mongodb cloud connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from "dotenv";
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
+dotenv.config();
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/demo'), UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        MongooseModule.forRoot(
+            `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.vqq92.mongodb.net/nestjs_integration_tests`,
+            {
+                autoCreate: true
+            }
+        ),
+        UsersModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
